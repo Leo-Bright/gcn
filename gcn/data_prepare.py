90db3ea746917e8a9636fa5fbb75442a80da8227
Leo Bright <564312023@qq.com>
2020-06-04 11:56:33 +0800

    增加node 的turning circle目录

--- gcn/data_prepare.py
+++ gcn/data_prepare.py
@@ -294,7 +294,7 @@ def gen_all_labeled_pkl(node2tag, node2idx, all_labeled_samples_path):
         if key in node2tag:
             all_labeled_samples_idx.append(node2idx[key])
             count_labeled += 1
-        elif count_unlabeled < count_labeled:
+        elif count_unlabeled < count_labeled + 1:
             all_labeled_samples_idx.append(node2idx[key])
             count_unlabeled += 1
 
@@ -340,7 +340,7 @@ if __name__ == '__main__':
     with open("sanfrancisco/sf_idx_node_dict.pkl", "rb") as f:
         idx_node_dict = pkl.load(f)
 
-    with open("sanfrancisco/osm_data/nodes_traffic_signals.json") as f:
+    with open("sanfrancisco/osm_data/nodes_turning_circle.json") as f:
         node_tag_dict = json.loads(f.readline())
 
     node_emb_dict = trans_input_file_to_ndarray('sanfrancisco/embeddings/sanfrancisco_raw_feature_none.embeddings')
@@ -361,15 +361,15 @@ if __name__ == '__main__':
     # for index in indexes_to_gen:
     #     red_idx = get_x_y_file(index[0], node_tag_dict, node_emb_dict, idx_node_dict, output=index[1:])
     #     if len(red_idx) > 0:
-    #         print(len(red_idx))
+    #         print('have redundat idx: ', len(red_idx))
     #         remove_redundant_node(network, red_idx, graph_file_path)
 
     # step2: generate otherx, othery file
-    # gcn_emb_idx_path = 'sanfrancisco/embeddings/sf_gcn_raw_feature_none_16d_traffic.embedding.idx.pkl'
+    # gcn_emb_idx_path = 'sanfrancisco/embeddings/sf_gcn_raw_feature_none_16d_turning_circle.embedding.idx.pkl'
     # idx_paths = [x_index[0], test_x_index[0], valid_x_index[0]]
     # red_idx = get_other_x_y_file(idx_paths, node_emb_dict, node_idx_dict, network)
     # if len(red_idx) > 0:
-    #     print(len(red_idx))
+    #     print('have redundat idx: ', len(red_idx))
     #     remove_redundant_node(network, red_idx, graph_file_path)
     # generate_global_idx(node_emb_dict, idx_node_dict, gcn_emb_idx_path)
 
@@ -380,13 +380,13 @@ if __name__ == '__main__':
     # under is not main process
     ########################################
 
-    # step0-0: generate the road network graph
-    # generate_network_graph('sanfrancisco/sf_roadnetwork', 'sanfrancisco/ind.sanfrancisco.graph', node_idx_dict)
-
-    # step0-1: generate the init indexes file to x, testx, validx
+    # step0-0: generate the init indexes file to x, testx, validx
     # node_idx_pkl_path = 'sanfrancisco/sf_node_idx_dict.pkl'
     # idx_node_pkl_path = 'sanfrancisco/sf_idx_node_dict.pkl'
     # gen_node_idx_pkl_path(node_emb_dict, node_idx_pkl_path, idx_node_pkl_path)
+
+    # step0-1: generate the road network graph
+    # generate_network_graph('sanfrancisco/sf_roadnetwork', 'sanfrancisco/ind.sanfrancisco.graph', node_idx_dict)
 
     # step0-2: generate idx pkl file of the all labeled samples that use to train/test/valid
     # all_labeled_pkl_path = 'sanfrancisco/ind.sanfrancisco.all.labeled.pkl'
@@ -394,7 +394,7 @@ if __name__ == '__main__':
 
     # step0-3: generate idx file of the all labeled samples that use to test
     # all_labeled_pkl_path = 'sanfrancisco/ind.sanfrancisco.all.labeled.pkl'
-    # samples_size = split_labeled_instance(all_labeled_pkl_path, 4000, 500)
+    # samples_size = split_labeled_instance(all_labeled_pkl_path, 1700, 200)
     # test_index_file_path = 'sanfrancisco/ind.sanfrancisco.test.index'
     # gen_test_index_file(samples_size, test_index_file_path)
 
--- gcn/test.py
+++ gcn/test.py
@@ -1,5 +1,6 @@
 import sys
 import numpy as np
+import json
 
 m = np.array([[1,1,1,1],
           [2,2,2,2],
@@ -10,3 +11,45 @@ print(m)
 print("======")
 m[[2,1],:] = m[[1,2],:]
 print(m)
+
+# with open('sanfrancisco/osm_data/sf_segments_tiger_nametype.json') as f:
+#     seg_tag_dict = json.loads(f.readline())
+#     tag_count = {}
+#     for key in seg_tag_dict:
+#         tag = seg_tag_dict[key]
+#         if tag not in tag_count:
+#             tag_count[tag] = 0
+#         tag_count[tag] += 1
+#
+#     for key in tag_count:
+#         print(key, tag_count[key])
+
+with open('sanfrancisco/sanfrancisco_nodes_with_all.tag') as f:
+
+    tag_count = {}
+    nodes = set()
+    result = {}
+
+    for line in f:
+        osmid_tag = line.strip().split(' ')
+        osmid = osmid_tag[0]
+        tag = osmid_tag[1]
+        if tag == '3':
+            result[osmid] = {'highway': 'turning_circle'}
+        if osmid in nodes:
+            continue
+        nodes.add(osmid)
+        if tag not in tag_count:
+            tag_count[tag] = 0
+        tag_count[tag] += 1
+
+    for key in tag_count:
+        print(key, tag_count[key])
+
+    # with open('sanfrancisco/osm_data/nodes_turning_circle.json', 'w+') as f:
+    #     json.dump(result, f)
+
+    circle_count = 0
+    for key in result:
+        circle_count += 1
+    print(circle_count)
